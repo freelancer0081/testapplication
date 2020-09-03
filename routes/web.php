4f73ff622735ea14a9c6e246674b1a8b38c1f88a
sahil <freelancer0081@gmail.com>
2020-09-03 12:10:32 +0530

    add vue and make frontend and backend file

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend');
+});
+Route::get('/backend', function () {
+    return view('backend');
 });
